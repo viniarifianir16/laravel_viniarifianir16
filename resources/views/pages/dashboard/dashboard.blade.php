98311f39bc82b88a3157fe93ef7ac9e601f2ea97
viniarifianir16 <viniarifianir16@gmail.com>
2025-09-03 00:44:30 +0700

    style: update style card welcome dashboard

--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -1,6 +1,43 @@
 <x-layouts.app :title="__('Dashboard')">
     <div>
-        <div class="flex h-full w-full flex-1 flex-col gap-4 mb-4 rounded-xl">
+        <div
+            class="flex flex-col lg:flex-row lg:items-center justify-between p-6 mb-6 text-white overflow-hidden rounded-xl border bg-gradient-to-br from-blue-500 to-cyan-500 dark:from-zinc-800 dark:to-zinc-900 dark:text-white dark:border-zinc-700 shadow-lg">
+
+            <!-- Left content -->
+            <div class="flex-1 grid gap-4">
+                <h1 class="text-3xl font-bold">Hello {{ auth()->user()->name }}!</h1>
+                <p class="text-lg">{{ now()->format('l, d M Y') }}</p>
+
+                <div class="flex flex-col sm:flex-row gap-6">
+                    <!-- Rumah Sakit -->
+                    <div
+                        class="flex items-center gap-3 w-50 bg-white/20 dark:bg-zinc-900/40 rounded-lg px-4 py-3 shadow-sm">
+                        <i class="bi bi-hospital text-3xl"></i>
+                        <div>
+                            <h4 class="text-xl font-bold">{{ $countHospitals }}</h4>
+                            <p class="text-md">Rumah Sakit</p>
+                        </div>
+                    </div>
+
+                    <!-- Pasien -->
+                    <div
+                        class="flex items-center gap-3 w-50 bg-white/20 dark:bg-zinc-900/40 rounded-lg px-4 py-3 shadow-sm">
+                        <i class="bi bi-person-raised-hand text-3xl"></i>
+                        <div>
+                            <h4 class="text-xl font-bold">{{ $countPatients }}</h4>
+                            <p class="text-md">Pasien</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Right image -->
+            <div class="hidden lg:block xl:block">
+                <img src="/image2.png" alt="Dashboard" class="w-100 h-auto">
+            </div>
+        </div>
+
+        {{-- <div class="flex h-full w-full flex-1 flex-col gap-4 mb-4 rounded-xl">
             <div class="grid grid-cols-2 auto-rows-min gap-4">
                 <div
                     class="relative grid auto-rows-min gap-3 grid-cols-3 overflow-hidden rounded-xl border bg-sky-200 border-neutral-200 dark:border-zinc-700 dark:bg-zinc-900">
@@ -23,7 +60,8 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
+
         @include ('components.cards.hospital-card')
     </div>
 </x-layouts.app>
